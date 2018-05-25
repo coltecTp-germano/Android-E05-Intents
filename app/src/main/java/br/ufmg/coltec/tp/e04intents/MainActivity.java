7e779abe4157d9e97f1804bd2e00ec703d273179
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-05-24 22:10:09 -0300

    Adding a barcode Exercise branch

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -2,10 +2,8 @@ package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     private static int FOTO_CODE = 1;
     TextView telephoneTextView;
-    TextView emailTextView;
+    TextView codeResultTextView;
     ImageView photoImageView;
     ImageButton photoButton;
 
@@ -23,36 +21,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        telephoneTextView = findViewById(R.id.telefone_tv);
-        emailTextView = findViewById(R.id.email_tv);
+        codeResultTextView = findViewById(R.id.codeResult_tv);
         photoImageView = findViewById(R.id.foto_iv);
         photoButton = findViewById(R.id.imageButton);
-
-        telephoneTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("tel:" + telephoneTextView.getText().toString());
-                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-                startActivity(intent);
-            }
-        });
-
-        emailTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "OI");
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-            }
-        });
 
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(fotoIntent, FOTO_CODE);
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+
+            startActivityForResult(intent, 0);
             }
         });
     }
@@ -60,9 +38,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            photoImageView.setImageBitmap(photo);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                // contents contains whatever was encoded
+                String contents = data.getStringExtra("SCAN_RESULT");
+
+                // Format contains the type of code i.e. UPC, EAN, QRCode etc...
+                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+
+                if (!TextUtils.isEmpty(contents)) {
+                    codeResultTextView.setText(contents);
+                }
+            }
         }
     }
 }
